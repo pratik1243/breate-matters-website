daea98c10e9d7229b0c517b194a0c573fa4f1ec7
pratik1243 <thakurpratik226@gmail.com>
2025-02-07 00:41:19 +0530

    website chnages 10

--- src/SectionComponents/UsersSection.jsx
+++ src/SectionComponents/UsersSection.jsx
@@ -5,10 +5,34 @@ import UserFeatureImg2 from "../assets/images/Health-practitioners-img.png";
 import UserFeatureImg3 from "../assets/images/athletes-performers-img.png";
 import UserFeatureImg4 from "../assets/images/healthcare-professionals-img.png";
 import UserFeatureImg5 from "../assets/images/educators-trainers-img.png";
+import gymLogo from "../assets/images/gym-equip-img.png";
 
 const UsersSection = () => {
   return (
     <div className="user-feature-section container">
+      <div className="gym-section">
+        <div>
+          <img src={gymLogo} className="gym-sec-bg" />
+        </div>
+        <div className="gym-desc-section">
+          <h2>What Makes the CapnoTrainer® go Different?</h2>
+
+          <p>
+            Unlike traditional breathing tools that focus on shallow techniques,
+            the CapnoTrainer® GO goes deeper. It integrates psychophysiology,
+            behavioral analysis, and cutting-edge technology to provide a
+            complete understanding of your breath’s role in health and
+            performance.
+          </p>
+
+          <div className="gym-desc-card">
+            “Breathing is behavior. And behavior can be changed. The
+            CapnoTrainer® helps you edit, optimize, and own your breath for
+            life.”
+          </div>
+        </div>
+      </div>
+
       <div className="user-feature-head-sec">
         <h2 className="head-txt">Who Is the CapnoTrainer® GO For?</h2>
       </div>
@@ -30,11 +54,9 @@ const UsersSection = () => {
         <UserFeatureCard
           image={UserFeatureImg3}
           heading={"Athletes & Performers"}
-          description={
-            "Boost endurance, focus, and physical resilience."
-          }
-        /> 
-         <UserFeatureCard
+          description={"Boost endurance, focus, and physical resilience."}
+        />
+        <UserFeatureCard
           image={UserFeatureImg4}
           heading={"Healthcare Professionals"}
           description={
@@ -47,7 +69,7 @@ const UsersSection = () => {
           description={
             "Incorporate evidence-based breathing techniques into your teaching or coaching."
           }
-        />     
+        />
       </div>
       {/* <div className="inner-user-feature-section section2">
       <UserFeatureCard
